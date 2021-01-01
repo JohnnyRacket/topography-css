de12a1cc6c60a04998d1d2160b4bfa32b87dbd49
johnnyracket <john.randolph.harrison@gmail.com>
2021-01-01 17:14:19 -0600

    introduced concept of size

--- example/src/Header.js
+++ example/src/Header.js
@@ -1,12 +1,12 @@
 export const Header = (props) => {
     return (
-      <div className="element " >
-        <div className="container" style={{display: "flex", flexDirection: "row", justifyContent: "space-between", paddingRight: '1.5rem'}}>
-          <div className="link margin-none padding" style={{fontWeight:'bold'}}>Example Page</div>
+      <div className="element ">
+        <div className="container flex row flex-center" style={{justifyContent: "space-between", paddingRight: '1.5rem', paddingLeft: '1.5rem'}}>
+          <div className="link margin-none padding-half large" style={{fontWeight:'bold'}}>Example Page</div>
           <div style={{display: "flex", flexDirection: "row"}}>
             <div className="input highlight margin-none padding">Home</div>
             <div className="input highlight margin-none padding">FAQ</div>
-            <button className="input accent semi-rounded" onClick={() => props.toggleTheme()}>Switch Theme</button>
+            <button className="input accent semi-rounded " onClick={() => props.toggleTheme()}>Switch Theme</button>
           </div>
         </div>
       </div>
