4a3b47267ac76b6dd71f1088b33c968c758a4d3e
johnnyracket <john.randolph.harrison@gmail.com>
2021-01-02 17:19:05 -0600

    moved example to npm package

--- example/src/index.js
+++ example/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './topography.css';
+// import './topography.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'topography-css';
 
 ReactDOM.render(
   <React.StrictMode>
