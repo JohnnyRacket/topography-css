3fede654982e3d95e17c74fb70c19f35536d2f28
johnnyracket <john.randolph.harrison@gmail.com>
2021-01-03 01:31:49 -0600

    updated docs to link to github and npm

--- example/src/Header.js
+++ example/src/Header.js
@@ -4,8 +4,8 @@ export const Header = (props) => {
         <div className="container flex row flex-center" style={{justifyContent: "space-between", paddingRight: '1.5rem', paddingLeft: '1.5rem'}}>
           <div className="link margin-none padding-half large title">topography css</div>
           <div className="flex row flex-center">
-            <div className="input highlight margin-none padding">Home</div>
-            <div className="input highlight margin-none padding">FAQ</div>
+            <a href="https://github.com/JohnnyRacket/topography-css"><div className="input highlight margin-none padding">Github</div></a>
+            <a href="https://www.npmjs.com/package/topography-css"><div className="input highlight margin-none padding">NPM</div></a>
             <button className="input accent semi-rounded " onClick={() => props.toggleTheme()}>Switch Theme</button>
           </div>
         </div>
