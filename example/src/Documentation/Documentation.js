4da44557f0fb4096505a4bb257f9c3af1384f099
johnnyracket <john.randolph.harrison@gmail.com>
2021-01-21 00:41:34 -0600

    fixed issue where syntax highlighting wouldnt work

--- example/src/Documentation/Documentation.js
+++ example/src/Documentation/Documentation.js
@@ -1,4 +1,4 @@
-import {Light as SyntaxHighlighter} from 'react-syntax-highlighter';
+import SyntaxHighlighter from 'react-syntax-highlighter';
 import {a11yDark} from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import {a11yLight} from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import {Surface} from './Surface'
